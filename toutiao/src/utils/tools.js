63d0018b97406142e1fc616ebd2457bd94ed41bd
Ray Yang <ryang@wfsaustralia.com>
2019-07-31 17:30:53 +1000

    add detect scroll position functions

--- toutiao/src/utils/tools.js
+++ toutiao/src/utils/tools.js
@@ -1,14 +1,54 @@
-function tplReplace(){
+function tplReplace() {
   return /{{(.*?)}}/g;
 }
-
-function thumbShow(dom){
-  dom.on('load',function(){
-    $(this).css('opacity',1);
+function thumbShow(dom) {
+  dom.on('load', function () {
+    $(this).css('opacity', 1);
   })
 }
-
-module.exports={
+function scrollToBottom(callback){
+  if(_getScrollTop()+_getWindowHeight()===_getScrollHeight()){
+    callback()
+  }
+}
+module.exports = {
   tplReplace,
   thumbShow,
+  scrollToBottom
+}
+
+//private function
+
+function _getScrollTop() {
+  let scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
+  if (document.body) {
+    bodyScrollTop = document.body.scrollTop;
+  }
+  if (document.documentElement) {
+    documentScrollTop = document.documentElement.scrollTop;
+  }
+  scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+  return scrollTop;
+}
+
+function _getScrollHeight() {
+  let scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
+  if (document.body) {
+    bodyScrollHeight = document.body.scrollHeight;
+  }
+  if (document.documentElement) {
+    documentScrollHeight = document.documentElement.scrollHeight;
+  }
+  scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+  return scrollHeight;
+}
+
+function _getWindowHeight() {
+  let windowHeight = 0
+  if (document.compatMode === 'CSS1Compat') {
+    windowHeight = document.documentElement.clientHeight
+  } else {
+    windowHeight = document.body.clientHeight;
+  }
+  return windowHeight;
 }
